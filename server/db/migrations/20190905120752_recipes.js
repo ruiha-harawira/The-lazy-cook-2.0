7c76db85e48ea04e05de93403dd4d04a70ccf888
Ruiha Harawira <103089870+ruiha-harawira@users.noreply.github.com>
2022-07-19 15:06:04 +1200

    fixed conflict

--- server/db/migrations/20190905120752_recipes.js
+++ server/db/migrations/20190905120752_recipes.js
@@ -1,16 +1,16 @@
 exports.up = function (knex) {
-  return knex.schema.createTable('recipes', table => {
-    table.increments('id')
-    table.string('name')
-    table.string('description')
-    table.string('img')
-    table.string('category')
-    table.string('ingredient1')
-    table.string('ingredient2')
-    table.string('ingredient3')
+  return knex.schema.createTable("recipes", (table) => {
+    table.increments("id")
+    table.string("name")
+    table.string("description", 500)
+    table.string("img")
+    table.string("category")
+    table.string("ingredient1")
+    table.string("ingredient2")
+    table.string("ingredient3")
   })
 }
 
 exports.down = function (knex) {
-  return knex.schema.dropTable('recipes')
+  return knex.schema.dropTable("recipes")
 }
